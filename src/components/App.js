600b0080fbf3dfb16c89f0f6792e060435c04aab
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-22 17:10:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,46 @@
 import React, { useState } from "react";
 import './../styles/App.css';
-import React from "react";
 
 const App = () => {
-    return (
+  const [todos, setTodos] = useState([]);        // State to hold the list of todos
+  const [inputValue, setInputValue] = useState("");  // State to hold the input field value
 
-        <div >
-           
-            <div className="contt">
-            <h1>To-do List</h1>
-            <div>
-                <input
-                    placeholder="Enter Todo"
-                    value={inputValue}
-                    onChange={(e) => setInputValue(e.target.value)}
-                />
-                <button onClick={addTodo}>Add Todo</button>
-                </div>
-            </div>
-            <ul className="tasks-list">
-                {
-                    tasks.map((task) => {
-                        return (
-                            <li className="task">
-                                <span>{task}</span>
-                                <button onClick={ () => removeTast(task)}>Delete</button>
-                            </li>
+  // Handler to add a new todo
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {  // Ensuring the input is not empty
+      setTodos([...todos, inputValue]);
+      setInputValue("");  // Clear input after adding
+    }
+  };
 
-                        );
-                    })
-                }
-            </ul>
+  // Handler to delete a todo
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
 
+  return (
+    <div>
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        // placeholder="Enter a task"
+      />
+      <button onClick={addTodo}>Add Todo</button>
 
-        </div>
-
-    )
-}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;

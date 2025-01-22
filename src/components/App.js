d729a4d90d139ca9dc32f3a401756981afd5bab5
Anna shabbagh <afreenshabbagh@gmail.com>
2025-01-22 17:07:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    return (
+
+        <div >
+           
+            <div className="contt">
+            <h1>To-do List</h1>
+            <div>
+                <input
+                    placeholder="Enter Todo"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <button onClick={addTodo}>Add Todo</button>
+                </div>
+            </div>
+            <ul className="tasks-list">
+                {
+                    tasks.map((task) => {
+                        return (
+                            <li className="task">
+                                <span>{task}</span>
+                                <button onClick={ () => removeTast(task)}>Delete</button>
+                            </li>
+
+                        );
+                    })
+                }
+            </ul>
+
+
+        </div>
+
+    )
 }
 
-export default App
+export default App;
